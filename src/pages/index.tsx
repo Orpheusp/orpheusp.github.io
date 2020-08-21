f92811e7857456ac6b04609be7013d02066a08ec
Orpheus Peng <stannolite@gmail.com>
2020-08-21 15:32:48 -0700

    Further set up the coding environment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
-export default function Home() {
+/** Root component that renders the entire site. */
+export default function Home(): JSX.Element {
   return <h1>Hello World!</h1>;
 }
