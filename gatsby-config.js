fdd713214bf2b97d5033c88da95f5d70c8995bfc
Orpheus Peng <stannolite@gmail.com>
2020-08-21 21:36:08 -0700

    Minor change to environment setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,11 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  siteMetadata: {
+    title: 'Orpheus Peng Personal Website',
+    siteUrl: 'https://www.orpheuspeng.com',
+    description:
+      'This is a personal website of Orpheus Peng, generated with Gatsby, ' +
+      'and hosted with GitHub Pages',
+  },
   plugins: [],
 };
