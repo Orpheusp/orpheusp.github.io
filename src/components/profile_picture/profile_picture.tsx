69099b7feff01eee5ae9466012c09407c3a03aa4
Orpheus Peng <stannolite@gmail.com>
2020-08-22 14:33:02 -0700

    Minor change to ProfilePicture

--- src/components/profile_picture/profile_picture.tsx
+++ src/components/profile_picture/profile_picture.tsx
@@ -5,10 +5,14 @@ import profilePic from '../../img/profile_pic.jpg';
 
 import './profile_picture.scss';
 
+interface Props {
+  className?: string;
+}
+
 /** A profile picture clipped. */
-export function ProfilePicture(): JSX.Element {
+export function ProfilePicture({ className }: Props): JSX.Element {
   return (
-    <div>
+    <div className={className}>
       <ProfilePicClip />
       <div className='profile-picture-clip--img-container'>
         <img className='profile-picture-clip--img' src={profilePic} />
