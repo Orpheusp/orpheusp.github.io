6817446c47827fa5e3240efc1f306ed677019df2
Orpheus Peng <stannolite@gmail.com>
2020-08-21 23:18:24 -0700

    Create NavBar sass file

--- src/components/nav_bar/nav_bar.tsx
+++ src/components/nav_bar/nav_bar.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
+import './nav_bar.scss';
+
 /** Component for the NavBar. */
 export function NavBar(): JSX.Element {
   return (
-    <nav className='navbar navbar-expand-lg'>
-      <div className='container'>
-        <a className='navbar-brand' href='#'>
+    <nav className='navbar navbar-expand-lg nav-bar'>
+      <div className='container nav-bar--container'>
+        <a className='navbar-brand nav-bar--logo' href='#'>
           Navbar
         </a>
       </div>
