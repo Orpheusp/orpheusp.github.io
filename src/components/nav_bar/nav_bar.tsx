b5d8c80ea07d9e2c137c136a2f20ea076a22478f
Orpheus Peng <stannolite@gmail.com>
2020-08-22 14:30:48 -0700

    Minor adjustment to NavBar

--- src/components/nav_bar/nav_bar.tsx
+++ src/components/nav_bar/nav_bar.tsx
@@ -9,7 +9,7 @@ export function NavBar(): JSX.Element {
   return (
     <nav className='navbar navbar-expand-lg nav-bar'>
       <div className='container nav-bar--container'>
-        <a className='navbar-brand nav-bar--logo' href='#'>
+        <a className='col-3 navbar-brand nav-bar--logo' href='#'>
           <img
             className='nav-bar--logo-img'
             src={logo}
