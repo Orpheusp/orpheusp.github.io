80f7d8dc686382e823894f3c28a91f046a1628b0
Orpheus Peng <stannolite@gmail.com>
2020-08-22 00:01:15 -0700

    Moved favicon related files back to static/ directory /shrug

--- src/html.tsx
+++ src/html.tsx
@@ -36,27 +36,23 @@ export default function Html(props: Props): JSX.Element {
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='/icon/apple-touch-icon.png'
+        href='/apple-touch-icon.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='/icon/favicon-32x32.png'
+        href='/favicon-32x32.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='16x16'
-        href='/icon/img/favicon-16x16.png'
+        href='/favicon-16x16.png'
       />
-      <link href='/icon/favicon.ico' rel='icon' type='image/x-icon' />
-      <link rel='manifest' href='/icon/site.webmanifest' />
-      <link
-        rel='mask-icon'
-        href='/icon/safari-pinned-tab.svg'
-        color='#2e597b'
-      />
+      <link href='/favicon.ico' rel='icon' type='image/x-icon' />
+      <link rel='manifest' href='/site.webmanifest' />
+      <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#2e597b' />
       <meta name='msapplication-TileColor' content='#2e597b' />
       <meta name='theme-color' content='#ffffff' />
     </React.Fragment>
