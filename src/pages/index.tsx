eb2caa7315aa620e18894ebc82e313a62cb00e0e
Orpheus Peng <stannolite@gmail.com>
2020-08-22 14:33:24 -0700

    Introduced IntroPane

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import './index.scss';
 
 import { NavBar } from '../components/nav_bar/nav_bar';
-import { ProfilePicture } from '../components/profile_picture/profile_picture';
+import { IntroPane } from '../components/intro_pane/intro_pane';
 
 /** Root component that renders the entire site. */
 export default function Home(): JSX.Element {
   return (
     <React.Fragment>
       <NavBar />
-      <ProfilePicture />
+      <IntroPane />
     </React.Fragment>
   );
 }
