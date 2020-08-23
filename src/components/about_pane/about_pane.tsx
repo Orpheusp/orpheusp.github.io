8ac3d987a63b77097621390a380f9f147682e55b
Orpheus Peng <stannolite@gmail.com>
2020-08-22 20:30:09 -0700

    Add PaneLable to AboutPane

--- src/components/about_pane/about_pane.tsx
+++ src/components/about_pane/about_pane.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import AboutPaneBackgroundClip from '../../img/about_pane_background_clip.inline.svg';
+import { PaneLabel } from '../pane_label/pane_label';
 
 import './about_pane.scss';
 
@@ -8,52 +8,51 @@ import './about_pane.scss';
 export function AboutPane(): JSX.Element {
   return (
     <div className='about-pane'>
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col-8 about-pane--text'>
-            <h6>
-              Thoughtful designs and user-empowering products have always
-              energized me. And as a software engineer, I have helped to bring
-              many of them to life with web technologies.
-            </h6>
-            <h6>
-              Beyond software development, I have a deep passion for art,
-              photography, and music. In my spare time, you will often find me
-              tinkering with Polaroid cameras 📸, meandering through art
-              exhibitions 🖼, attending orchestra concerts 🎻, or maybe just at
-              home watering my plants 🌱.
-            </h6>
-            <h6>
-              If you’d like to, check out my{' '}
-              <a
-                href='https://linkedin.com/in/weipeng29'
-                target='_blank'
-                className='text-bold'
-                rel='noreferrer'
-              >
-                LinkedIn
-              </a>{' '}
-              profile , get on{' '}
-              <a
-                href='https://github.com/orpheusp'
-                target='_blank'
-                className='text-bold'
-                rel='noreferrer'
-              >
-                GitHub
-              </a>{' '}
-              to see what I have been up to, or take a look at my{' '}
-              <a
-                href='/orpheus_peng_resume.pdf'
-                target='_blank'
-                className='text-bold'
-                rel='noreferrer'
-              >
-                resume
-              </a>
-              .
-            </h6>
-          </div>
+      <div className='container about-pane--text'>
+        <PaneLabel text='about' className='offset-2' />
+        <div className='col-8 offset-2'>
+          <h6>
+            Thoughtful designs and user-empowering products have always
+            energized me. And as a software engineer, I have helped to bring
+            many of them to life with web technologies.
+          </h6>
+          <h6>
+            Beyond software development, I have a deep passion for art,
+            photography, and music. In my spare time, you will often find me
+            tinkering with Polaroid cameras 📸, meandering through art
+            exhibitions 🖼, attending orchestra concerts 🎻, or maybe just at
+            home watering my plants 🌱.
+          </h6>
+          <h6>
+            If you’d like to, check out my{' '}
+            <a
+              href='https://linkedin.com/in/weipeng29'
+              target='_blank'
+              className='text-bold'
+              rel='noreferrer'
+            >
+              LinkedIn
+            </a>{' '}
+            profile , get on{' '}
+            <a
+              href='https://github.com/orpheusp'
+              target='_blank'
+              className='text-bold'
+              rel='noreferrer'
+            >
+              GitHub
+            </a>{' '}
+            to see what I have been up to, or take a look at my{' '}
+            <a
+              href='/orpheus_peng_resume.pdf'
+              target='_blank'
+              className='text-bold'
+              rel='noreferrer'
+            >
+              resume
+            </a>
+            .
+          </h6>
         </div>
       </div>
     </div>
--- src/components/pane_label/pane_label.tsx
+++ src/components/pane_label/pane_label.tsx
@@ -3,10 +3,13 @@ import * as React from 'react';
 import './pane_label.scss';
 
 interface Props {
+  className?: string;
   text: string;
 }
 
 /** A label for a pane */
-export function PaneLabel({ text }: Props): JSX.Element {
-  return <div className='subtitle-1 col-2 pane-label'>{text}</div>;
+export function PaneLabel({ text, className }: Props): JSX.Element {
+  return (
+    <div className={`subtitle-1 col-2 pane-label ${className}`}>{text}</div>
+  );
 }
