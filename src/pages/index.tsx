f0f24acb36566b4abc7fc862fd92ac2c693224cf
Orpheus Peng <stannolite@gmail.com>
2020-08-22 21:29:30 -0700

    Create ExperiencesPane and introduce it into the front page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import './index.scss';
 
 import { AboutPane } from '../components/about_pane/about_pane';
 import { AboutPaneBackground } from '../components/about_pane_background/about_pane_background';
+import { ExperiencesPane } from '../components/experiences_pane/experiences_pane';
 import { IntroPane } from '../components/intro_pane/intro_pane';
 import { NavBar } from '../components/nav_bar/nav_bar';
 
@@ -17,6 +18,7 @@ export default function Home(): JSX.Element {
         <AboutPaneBackground className='pane-background--background' />
       </div>
       <AboutPane />
+      <ExperiencesPane />
     </React.Fragment>
   );
 }
